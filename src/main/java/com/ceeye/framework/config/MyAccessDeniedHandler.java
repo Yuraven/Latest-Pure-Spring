8871ffc7dff44a516f6579bdf9506e08ab2e3625
yuruiwen <yuruiwen@outlook.com>
2024-01-19 18:11:15 +0800

    修改

--- src/main/java/com/ceeye/framework/config/MyAccessDeniedHandler.java
+++ src/main/java/com/ceeye/framework/config/MyAccessDeniedHandler.java
@@ -1,8 +1,8 @@
 package com.ceeye.framework.config;
 
-import com.alibaba.fastjson2.JSON;
 import com.ceeye.framework.enums.ResultEnum;
 import com.ceeye.framework.vo.ResultVO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.access.AccessDeniedException;
@@ -14,14 +14,16 @@ import java.io.IOException;
 
 /**
  * 处理无权登录的情况
+ *
  * @author : raven
  * @since : 2020-02-12 15:26
  */
 @Component
 public class MyAccessDeniedHandler implements AccessDeniedHandler {
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException{
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         response.setContentType("text/json;charset=UTF-8");
-        response.getWriter().write(JSON.toJSONString(new ResultVO<>(ResultEnum.USER_NO_ACCESS,false)));
+        ObjectMapper mapper = new ObjectMapper();
+        response.getWriter().write(mapper.writeValueAsString(new ResultVO<>(ResultEnum.USER_NO_ACCESS, false)));
     }
 }
